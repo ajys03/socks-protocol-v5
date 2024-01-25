2ff8de5b348ca60d0a67573ad29018b342ccb655
ajys <ajys@umich.edu>
2024-01-25 15:23:06 -0500

    establish sub-negotiation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # socks-protocol-v5
-SOCKS Protocol Version 5
+[SOCKS Protocol Version 5](https://pages.github.com/).
+
+Currently, skipping the negotiation for the authentication method in Procedure for TCP-based clients. 
--- main.cpp
+++ main.cpp
@@ -4,8 +4,44 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-void handle_method_negotiation(int client_socket) {
+#define BUFFER_SIZE 4096
+
+int handle_method_negotiation(const int client_socket) {
+    char buffer[BUFFER_SIZE];
+    // maybe ssize_t -> https://stackoverflow.com/questions/131803/unsigned-int-vs-size-t
+    // octect means 8 bits (1 byte) -> spec writes X'hh' (8 bits) to denote the value of the single octet
+    int bytes_recv = recv(client_socket, buffer, 2, 0); // recv 2 bytes for ver & nmethods
     
+    // protocol verion 5
+    if (bytes_recv != 2 || buffer[0] == 0x05) {
+        std::cerr << "Invalid SOCKS version or number of methods" << std::endl;
+        close(client_socket);
+        return;
+    }
+
+    int n_methods = static_cast<int>(buffer[1]);
+    // Read the methods
+    bytes_recv = recv(client_socket, buffer, n_methods, 0);
+    if (bytes_recv != n_methods) {
+        std::cerr << "Error reading methods" << std::endl;
+        close(client_socket);
+        return;
+    }
+
+    // Use 'NO AUTHENTICATION REQUIRED' (X'00') but first check if it is supported
+    int i = 0;
+    while (buffer[i] != 0x00) {
+        if (i == n_methods - 1) {
+            send(client_socket, "\x05\xff", 2, 0);
+            close(client_socket);
+            return -1;
+        }
+        i++;
+    }
+
+    // Send the selected method (X'00' for 'NO AUTHENTICATION REQUIRED')
+    send(client_socket, "\x05\x00", 2, 0);
+    return 0;
 }
 
 int main() {
@@ -56,10 +92,15 @@ int main() {
 
         std::cout << "Accepted connection from " << inet_ntoa(client_address.sin_addr) << std::endl;
 
-        handle_method_negotiation(client_socket);
+        int neg_success = handle_method_negotiation(client_socket);
+        if (neg_success < 0) {
+            std::cerr << "Error negotiating methods" << std::endl;
+            close(server_fd);
+            return -1;
+        }
+
+        // REQUESTS
         
-        // add wait time
-        sleep(1);
     }
 
     close(server_fd);
